bd1e17c3ed7b2feb966ba6a6ad485da5627ba1bd
yunnaidan <yunnaidan@pku.edu.cn>
2019-12-17 18:06:06 +0800

    modified

--- dyntrigger/utils/cal_distance.py
+++ dyntrigger/utils/cal_distance.py
@@ -1,20 +1,23 @@
 from math import radians, cos, sin, asin, sqrt
-def haversine(lon1, lat1, lon2, lat2):  # 经度1，纬度1，经度2，纬度2 （十进制度数）
+
+
+def haversine(lon1, lat1, lon2, lat2):  # degree
     """
     Calculate the great circle distance between two points
     on the earth (specified in decimal degrees)
     """
-    # 将十进制度数转化为弧度
+    # Translate degree to radian.
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
 
-    # haversine公式
+    # Haversine formula.
     dlon = lon2 - lon1
     dlat = lat2 - lat1
     a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
     c = 2 * asin(sqrt(a))
-    r = 6371  # 地球平均半径，单位为公里
-    return c*r,c*r/111
+    r = 6371  # Radius of the Earth.
+    return c * r, c * r / 111
 
-if __name__== "__main__":
-    print (haversine(103,26.5,-76.36,1.93))
-    print ('finish')
+
+if __name__ == "__main__":
+    print(haversine(103, 26.5, -76.36, 1.93))
+    print('finish')
